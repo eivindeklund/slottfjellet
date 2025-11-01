b8513a7f31df05ee4a49c027f9188c4fdcbeb356
Eivind Eklund <eeklund@gmail.com>
2025-11-01 12:53:23 +0100

    Refactor: Move all the input files into src/
    This is to make the workspace easier to navigate.
    
    The generated _site is byte-by-byte identical with the one generated before this change.

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,15 +2,31 @@ module.exports = function (eleventyConfig) {
 	// Input directory: src
 	// Output directory: _site
 
-	//eleventyConfig.addPassthroughCopy(["*.png", "*.css", "favicon.ico", "site.webmanifest"]);
-	eleventyConfig.addPassthroughCopy("*.png");
-	eleventyConfig.addPassthroughCopy("*.css");
-	eleventyConfig.addPassthroughCopy("*.svg");
-	eleventyConfig.addPassthroughCopy("*.jpg");
-	eleventyConfig.addPassthroughCopy("favicon.ico");
-	eleventyConfig.addPassthroughCopy("site.webmanifest");
-	eleventyConfig.addPassthroughCopy("favicon/*");
-	eleventyConfig.addPassthroughCopy("js/*.js");
+	//
+	// Static files to pass through to output
+	//
+
+	// TODO: Move these to img/ and css/ and passthrough those folders
+	eleventyConfig.addPassthroughCopy("src/*.png");
+	eleventyConfig.addPassthroughCopy("src/*.css");
+	eleventyConfig.addPassthroughCopy("src/*.svg");
+	eleventyConfig.addPassthroughCopy("src/*.jpg");
+	// Stuff that must be in root
+	eleventyConfig.addPassthroughCopy("src/favicon.ico");
+	eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+	// Directories
+	eleventyConfig.addPassthroughCopy("src/favicon");
+	eleventyConfig.addPassthroughCopy("src/js");
+	// TODO: .htaccess is probably not necessary (it just sets js files to
+	// be the right mime type, and likely the server does that by itself)
+	eleventyConfig.addPassthroughCopy("src/js/.htaccess");
 	// Pass through during --serve, the default is "copy"
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+	// Tell Eleventy to use src/ as its input directory
+	return {
+		dir: {
+			input: "src",
+			output: "_site"
+		}
+	};
 };
