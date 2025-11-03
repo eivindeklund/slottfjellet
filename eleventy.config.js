7667b672e999c4c5585814416e135c676c910f31
Eivind Eklund <eeklund@gmail.com>
2025-11-03 16:38:03 +0100

    Omorganiser så bare HTML-filer er direkte i src/
    Bilder flyttes til src/images, files from skal direkte i / puttes i src/_extra_for_root.

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,17 +7,16 @@ module.exports = function (eleventyConfig) {
 	//
 
 	// TODO: Move these to img/ and css/ and passthrough those folders
-	eleventyConfig.addPassthroughCopy("src/*.png");
-	eleventyConfig.addPassthroughCopy("src/*.css");
-	eleventyConfig.addPassthroughCopy("src/*.svg");
-	eleventyConfig.addPassthroughCopy("src/*.jpg");
 	// Stuff that must be in root
 	eleventyConfig.addPassthroughCopy("src/favicon.ico");
 	eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 	// Directories
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/favicon");
+	eleventyConfig.addPassthroughCopy("src/images");
 	eleventyConfig.addPassthroughCopy("src/js");
+	// Extra files to be in root
+	eleventyConfig.addPassthroughCopy({ "src/_extra_for_root": "/" });
 	// TODO: .htaccess is probably not necessary (it just sets js files to
 	// be the right mime type, and likely the server does that by itself)
 	eleventyConfig.addPassthroughCopy("src/js/.htaccess");
